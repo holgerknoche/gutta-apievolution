4d84da577639091b2cb708e464cbc1313d1a8c7e
Holger Knoche <hkn@informatik.uni-kiel.de>
2022-10-13 22:40:41 +0200

    Improved consistency checks

--- gutta-apievolution-core/src/main/java/gutta/apievolution/core/resolution/ProviderToConsumerMap.java
+++ gutta-apievolution-core/src/main/java/gutta/apievolution/core/resolution/ProviderToConsumerMap.java
@@ -1,23 +1,22 @@
 package gutta.apievolution.core.resolution;
 
 import gutta.apievolution.core.apimodel.ApiDefinitionMorphism;
-import gutta.apievolution.core.apimodel.EnumType;
-import gutta.apievolution.core.apimodel.Field;
 import gutta.apievolution.core.apimodel.Optionality;
-import gutta.apievolution.core.apimodel.RecordType;
 import gutta.apievolution.core.apimodel.Type;
 import gutta.apievolution.core.apimodel.TypeMap;
-import gutta.apievolution.core.apimodel.TypeVisitor;
 import gutta.apievolution.core.apimodel.Usage;
+import gutta.apievolution.core.apimodel.UserDefinedType;
 import gutta.apievolution.core.apimodel.consumer.ConsumerApiDefinition;
 import gutta.apievolution.core.apimodel.consumer.ConsumerEnumMember;
 import gutta.apievolution.core.apimodel.consumer.ConsumerField;
 import gutta.apievolution.core.apimodel.consumer.ConsumerOperation;
+import gutta.apievolution.core.apimodel.consumer.ConsumerRecordType;
 import gutta.apievolution.core.apimodel.consumer.ConsumerUserDefinedType;
 import gutta.apievolution.core.apimodel.provider.ProviderApiDefinition;
 import gutta.apievolution.core.apimodel.provider.ProviderEnumMember;
 import gutta.apievolution.core.apimodel.provider.ProviderField;
 import gutta.apievolution.core.apimodel.provider.ProviderOperation;
+import gutta.apievolution.core.apimodel.provider.ProviderRecordType;
 import gutta.apievolution.core.apimodel.provider.ProviderUserDefinedType;
 import gutta.apievolution.core.util.CheckResult;
 
@@ -68,63 +67,37 @@ class ProviderToConsumerMap extends ApiDefinitionMorphism<ProviderApiDefinition,
     protected CheckResult checkConsistency() {
         CheckResult superResult = super.checkConsistency();
         
-        this.checkTypeAssociation();
+        CheckResult ownResult = new CheckResult();
+        this.ensureMandatoryFieldsAreMapped(ownResult);
         
-        // TODO Join with own result
-        return superResult;
+        return superResult.joinWith(ownResult);
     }
-
-    private void checkTypeAssociation() {
-        ProviderTypeConsistencyChecker checker = new ProviderTypeConsistencyChecker();
-
-        this.typeMap.forEach(checker::checkConsistency);
-    }
-
-    private class ProviderTypeConsistencyChecker implements TypeVisitor<Void> {
-
-        private Type foreignType;
-
-        public void checkConsistency(Type ownType, Type foreignType) {
-            this.foreignType = foreignType;
-            ownType.accept(this);
-        }
-
-        private ConsumerField resolveForeignField(ProviderField ownField) {
-            return ProviderToConsumerMap.this.fieldMap.get(ownField);
-        }
-
-        @Override
-        public Void handleEnumType(EnumType<?, ?, ?> enumType) {
-            // No specific checks as of now
-            return null;
-        }
-
-        @Override
-        public Void handleRecordType(RecordType<?, ?, ?> recordType) {
-            RecordType<?, ?, ?> foreignRecordType = (RecordType<?, ?, ?>) this.foreignType;
-
-            // Assert that the types of the fields are compatible
-            for (Field<?, ?> field : recordType.getDeclaredFields()) { // NOSONAR Type is correct
-                ProviderField ownField = (ProviderField) field;
-                ConsumerField foreignField = this.resolveForeignField(ownField);
-                this.checkField(ownField, foreignField, foreignRecordType);
-            }
-
-            return null;
-        }
-
-        private void checkField(ProviderField ownField, ConsumerField foreignField, RecordType<?, ?, ?> foreignType) {
-            Optionality ownOptionality = ownField.getOptionality();
-
-            // Determine the usage by the consumer, since output-only types do not have to be mapped
-            Usage usage = foreignType.getUsage();
-            
-            if (foreignField == null && ownOptionality == Optionality.MANDATORY && usage != Usage.OUTPUT) {
-                // Report an error if a mandatory field is not mapped and is not used only for output
-                throw new DefinitionResolutionException("Non-optional field " + ownField + " is not mapped.");
+    
+    private void ensureMandatoryFieldsAreMapped(CheckResult result) {
+        for (UserDefinedType<?> udt : this.sourceDefinition.getUserDefinedTypes()) {
+            if (udt instanceof ProviderRecordType) {
+                ProviderRecordType providerType = (ProviderRecordType) udt;
+                ConsumerRecordType consumerType = (ConsumerRecordType) this.mapUserDefinedType(providerType).orElse(null);
+                
+                for (ProviderField field : providerType) {
+                    this.ensureMandatoryFieldIsMapped(field, consumerType, result);
+                }
             }
         }
+    }
+    
+    private void ensureMandatoryFieldIsMapped(ProviderField providerField, ConsumerRecordType consumerType,
+            CheckResult result) {
+        ConsumerField consumerField = this.mapProviderField(providerField);
+        Optionality providerOptionality = providerField.getOptionality();
 
+        // Determine the usage by the consumer, since output-only types do not have to be mapped
+        Usage usage = consumerType.getUsage();
+        
+        if (consumerField == null && providerOptionality == Optionality.MANDATORY && usage != Usage.OUTPUT) {
+            // Report an error if a mandatory field is not mapped and is not used only for output
+            result.addErrorMessage("Non-optional field " + providerField + " is not mapped.");
+        }
     }
 
 }
