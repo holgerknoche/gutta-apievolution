db58e15bf66ac170603246a252414fc269bc3eb0
Holger Knoche <hkn@informatik.uni-kiel.de>
2023-11-02 12:50:35 +0100

    Added some comments on proxy generation

--- gutta-apievolution-inprocess/src/main/java/gutta/apievolution/inprocess/dynproxy/DynamicProxyFactory.java
+++ gutta-apievolution-inprocess/src/main/java/gutta/apievolution/inprocess/dynproxy/DynamicProxyFactory.java
@@ -7,6 +7,13 @@ import gutta.apievolution.inprocess.UDTToClassMap;
 
 import java.lang.reflect.Proxy;
 
+// Comments on dynamic proxies (esp. vs. explicit object conversion)
+// - Proxy creation is not free, though may be cheaper if few attributes are actually read
+// - Interface requirement is cumbersome for exceptions
+// - equals() is either identity or inherited from the implementation. In the latter case,
+//   an object that is indistinguishable from the consumer perspective may not be equal due
+//   to fields only visible to the provider
+
 public class DynamicProxyFactory implements ProxyFactory {
 
     @Override
