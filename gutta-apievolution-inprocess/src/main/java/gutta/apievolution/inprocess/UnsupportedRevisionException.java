c057d2c8dbcd7258cc8efece995579227e54c949
Holger Knoche <hkn@informatik.uni-kiel.de>
2023-11-30 22:44:07 +0100

    JavaDoc

--- gutta-apievolution-inprocess/src/main/java/gutta/apievolution/inprocess/UnsupportedRevisionException.java
+++ gutta-apievolution-inprocess/src/main/java/gutta/apievolution/inprocess/UnsupportedRevisionException.java
@@ -7,7 +7,13 @@ public class UnsupportedRevisionException extends RuntimeException {
 
     private static final long serialVersionUID = -5518977849903584494L;
 
-    UnsupportedRevisionException(int requestedRevision, int[] supportedRevisions) {
+    /**
+     * Creates a new exception using the given data.
+     * 
+     * @param requestedRevision  The requested, unsupported revision number
+     * @param supportedRevisions The supported revision numbers
+     */
+    public UnsupportedRevisionException(int requestedRevision, int[] supportedRevisions) {
         super(String.format("Revision %d is not supported. Supported revisions are: %d.", requestedRevision, supportedRevisions));
     }
 
