844f533c14c10cba90b06a8e198caeff7c4ca7d6
Holger Knoche <hkn@informatik.uni-kiel.de>
2023-12-11 22:59:15 +0100

    Switched from methods to method handles

--- gutta-apievolution-fixedformat/src/main/java/gutta/apievolution/fixedformat/objectmapping/FieldMapper.java
+++ gutta-apievolution-fixedformat/src/main/java/gutta/apievolution/fixedformat/objectmapping/FieldMapper.java
@@ -1,40 +1,49 @@
 package gutta.apievolution.fixedformat.objectmapping;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.reflect.Method;
 
 class FieldMapper {
 
-    private final Method readAccessor;
-    
-    private final Method writeAccessor;
-    
+    private final MethodHandle readAccessor;
+
+    private final MethodHandle writeAccessor;
+
     private final TypeMapper<?> typeMapper;
-    
+
     public FieldMapper(Method readAccessor, Method writeAccessor, TypeMapper<?> typeMapper) {
-        this.readAccessor = readAccessor;
-        this.writeAccessor = writeAccessor;
+        Lookup lookup = MethodHandles.publicLookup();
+
+        try {
+            this.readAccessor = lookup.unreflect(readAccessor);
+            this.writeAccessor = lookup.unreflect(writeAccessor);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
         this.typeMapper = typeMapper;
     }
-    
+
     public int getMaxLength() {
         return this.typeMapper.getMaxLength();
     }
-    
+
     public void readValue(FixedFormatData data, Object target) {
         try {
             Object value = this.typeMapper.readValue(data);
             this.writeAccessor.invoke(target, value);
-        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        } catch (Throwable e) {
             throw new RuntimeException(e);
         }
     }
-    
+
     public void writeValue(Object object, FixedFormatData data) {
         try {
             Object fieldValue = this.readAccessor.invoke(object);
             this.typeMapper.writeValue(fieldValue, data);
-        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        } catch (Throwable e) {
             throw new RuntimeException(e);
         }
     }
