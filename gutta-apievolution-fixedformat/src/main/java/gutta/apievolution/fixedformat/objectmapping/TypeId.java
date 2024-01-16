c2feed03707adf6de581fbc6b37aa16a1acac605
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-01-16 22:19:37 +0100

    Addressed CheckStyle issues

--- gutta-apievolution-fixedformat/src/main/java/gutta/apievolution/fixedformat/objectmapping/TypeId.java
+++ gutta-apievolution-fixedformat/src/main/java/gutta/apievolution/fixedformat/objectmapping/TypeId.java
@@ -6,10 +6,19 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * This annotation is used to specify the type id in the fixed data format on a
+ * representation type.
+ */
 @Retention(RUNTIME)
 @Target(TYPE)
 public @interface TypeId {
     
+    /**
+     * The type id of the annotated type.
+     * 
+     * @return see above
+     */
     int value();
 
 }
