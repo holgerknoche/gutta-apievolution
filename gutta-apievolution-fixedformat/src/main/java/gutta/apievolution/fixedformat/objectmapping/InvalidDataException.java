71b0241302e0d7e7435703f371ebf30a502afbe1
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-01-31 23:17:36 +0100

    Added serialVersionUID

--- gutta-apievolution-fixedformat/src/main/java/gutta/apievolution/fixedformat/objectmapping/InvalidDataException.java
+++ gutta-apievolution-fixedformat/src/main/java/gutta/apievolution/fixedformat/objectmapping/InvalidDataException.java
@@ -2,6 +2,8 @@ package gutta.apievolution.fixedformat.objectmapping;
 
 class InvalidDataException extends RuntimeException {
     
+    private static final long serialVersionUID = 7827157446187524482L;
+
     public InvalidDataException(String message) {
         super(message);
     }
