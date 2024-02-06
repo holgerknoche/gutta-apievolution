f99a4b45d32ebe3b3971ea83fb6b9d8f68ae92ae
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-02-06 06:41:04 +0100

    Made consumer proxies public

--- gutta-apievolution-customerexample/src/main/java/gutta/apievolution/customerexample/json/consumer/v3/UpsertOperationConsumerProxyV3.java
+++ gutta-apievolution-customerexample/src/main/java/gutta/apievolution/customerexample/json/consumer/v3/UpsertOperationConsumerProxyV3.java
@@ -4,7 +4,7 @@ import gutta.apievolution.core.apimodel.consumer.ConsumerApiDefinition;
 import gutta.apievolution.json.ConsumerOperationProxy;
 import gutta.apievolution.json.RequestRouter;
 
-class UpsertOperationConsumerProxyV3 extends ConsumerOperationProxy<Customer, Customer> {
+public class UpsertOperationConsumerProxyV3 extends ConsumerOperationProxy<Customer, Customer> {
 
 	public UpsertOperationConsumerProxyV3(ConsumerApiDefinition consumerApi, String apiId, RequestRouter router) {
 	    super(consumerApi, apiId, "upsert", "Customer", "Customer", Customer.class, router);
