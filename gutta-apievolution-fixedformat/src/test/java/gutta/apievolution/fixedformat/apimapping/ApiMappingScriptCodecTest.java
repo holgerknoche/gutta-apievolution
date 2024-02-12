e9548c388fafa7b38e296248fd74cdbfdf0fde3c
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-02-12 07:01:12 +0100

    Created test case for API mapping scripts

---
+++ gutta-apievolution-fixedformat/src/test/java/gutta/apievolution/fixedformat/apimapping/ApiMappingScriptCodecTest.java
@@ -0,0 +1,12 @@
+package gutta.apievolution.fixedformat.apimapping;
+
+import org.junit.jupiter.api.Test;
+
+class ApiMappingScriptCodecTest {
+    
+    @Test
+    void simpleMappingScript() {
+        // TODO
+    }
+
+}
