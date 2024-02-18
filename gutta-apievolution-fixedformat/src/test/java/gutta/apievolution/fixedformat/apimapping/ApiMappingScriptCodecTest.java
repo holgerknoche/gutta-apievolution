3af3dc2558e0a47caab7f54f763547e12ca8757d
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-02-18 17:09:37 +0100

    More tests

--- gutta-apievolution-fixedformat/src/test/java/gutta/apievolution/fixedformat/apimapping/ApiMappingScriptCodecTest.java
+++ gutta-apievolution-fixedformat/src/test/java/gutta/apievolution/fixedformat/apimapping/ApiMappingScriptCodecTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -162,6 +163,128 @@ class ApiMappingScriptCodecTest {
                 0x00, 0x00, 0x00, 0x06, // Source type id of the second mapping
                 0x00, 0x00, 0x00, 0x01, // Target type id of the second mapping
                 0x00, 0x00, 0x00, 0x01 // Type index of the second mapping
+        };
+        
+        byte[] scriptBytes = codec.encodeScript(script);
+        assertArrayEquals(expectedBytes, scriptBytes);
+        
+        ApiMappingScript decodedScript = codec.decodeScript(scriptBytes);
+        assertEquals(script, decodedScript);
+    }
+    
+    /**
+     * Test case: Serialization and deserialization of a script where a polymorphic type is used within a record type.
+     */
+    @Test
+    void scriptWithEmbeddedPolymorphicType() {
+        // First subtype
+        FieldMapping fieldAMapping = new FieldMapping(0, new CopyOperation(5));
+        RecordTypeEntry typeAEntry = new RecordTypeEntry(0, 0, 5, singletonList(fieldAMapping));
+        
+        // Second subtype
+        FieldMapping fieldBMapping = new FieldMapping(0, new CopyOperation(10));
+        RecordTypeEntry typeBEntry = new RecordTypeEntry(1, 1, 10, singletonList(fieldBMapping));
+        
+        PolymorphicRecordMapping typeAMapping = new PolymorphicRecordMapping(5, 0, typeAEntry);
+        PolymorphicRecordMapping typeBMapping = new PolymorphicRecordMapping(6, 1, typeBEntry); 
+        PolymorphicRecordMappingOperation polyMappingOperation = new PolymorphicRecordMappingOperation(Arrays.asList(typeAMapping, typeBMapping));
+        
+        // Type containing the polymorphic mapping
+        FieldMapping polyFieldMapping = new FieldMapping(0, polyMappingOperation);
+        RecordTypeEntry containerTypeEntry = new RecordTypeEntry(2, 2, 10, singletonList(polyFieldMapping));
+        
+        OperationEntry operationEntry = new OperationEntry(0, "op", new RecordMappingOperation(containerTypeEntry), new RecordMappingOperation(containerTypeEntry));
+        
+        ApiMappingScript script = new ApiMappingScript(asList(typeAEntry, typeBEntry, containerTypeEntry), singletonList(operationEntry));
+        ApiMappingScriptCodec codec = new ApiMappingScriptCodec();
+        
+        byte[] expectedBytes = new byte[] {
+                // --- Type entries
+                0x00, 0x00, 0x00, 0x03, // Number of type entries
+                0x00, 0x00, 0x00, 0x14, // Offset of the first type entry
+                0x00, 0x00, 0x00, 0x2A, // Offset of the second type entry
+                0x00, 0x00, 0x00, 0x40, // Offset of the third type entry
+                0x00, 0x00, 0x00, 0x6E, // Offset of the operation list
+                0x02, // Entry type for the first type (record type)
+                0x00, 0x00, 0x00, 0x00, // Type id of the first type
+                0x00, 0x00, 0x00, 0x05, // Data size of the first type
+                0x00, 0x00, 0x00, 0x01, // Number of field mappings
+                0x00, 0x00, 0x00, 0x00, // Offset of the first field
+                0x01, // Operation type of the mapping (copy)
+                0x00, 0x00, 0x00, 0x05, // Number of bytes to copy
+                0x02, // Entry type for the second type (record type)
+                0x00, 0x00, 0x00, 0x01, // Type id of the second type
+                0x00, 0x00, 0x00, 0x0A, // Data size of the second type
+                0x00, 0x00, 0x00, 0x01, // Number of field mappings
+                0x00, 0x00, 0x00, 0x00, // Offset of the first field
+                0x01, // Operation type of the mapping (copy)
+                0x00, 0x00, 0x00, 0x0A, // Number of bytes to copy
+                0x02, // Entry type for the third type (record type)
+                0x00, 0x00, 0x00, 0x02, // Type id of the third type
+                0x00, 0x00, 0x00, 0x0A, // Data size of the third type
+                0x00, 0x00, 0x00, 0x01, // Number of field mappings
+                0x00, 0x00, 0x00, 0x00, // Offset of the first field
+                0x06, // Operation type of the mapping (poly record mapping)
+                0x00, 0x00, 0x00, 0x02, // Number of type mappings
+                0x00, 0x00, 0x00, 0x05, // Source type id of the first mapping
+                0x00, 0x00, 0x00, 0x00, // Target type id of the first mapping
+                0x00, 0x00, 0x00, 0x00, // Type index of the first mapping
+                0x00, 0x00, 0x00, 0x06, // Source type id of the second mapping
+                0x00, 0x00, 0x00, 0x01, // Target type id of the second mapping
+                0x00, 0x00, 0x00, 0x01, // Type index of the second mapping
+                // --- Operation entries
+                0x00, 0x00, 0x00, 0x01, // Number of operation entries
+                0x00, 0x00, 0x00, 0x02, // Length of the operation name (in bytes)
+                0x6F, 0x70, // Name of the operation
+                0x04, // Operation type of the parameter mapping operation (record mapping)
+                0x00, 0x00, 0x00, 0x02, // Type index of the record
+                0x04, // Operation type of the result mapping operation (record mapping)
+                0x00, 0x00, 0x00, 0x02 // Type index of the record
+        };
+        
+        byte[] scriptBytes = codec.encodeScript(script);
+        assertArrayEquals(expectedBytes, scriptBytes);
+        
+        ApiMappingScript decodedScript = codec.decodeScript(scriptBytes);
+        assertEquals(script, decodedScript);
+    }
+    
+    /**
+     * Test case: Serialization and deserialization of a script with multiple operations.
+     */
+    @Test
+    void scriptWithMultipleOperations() {
+        RecordTypeEntry typeEntry = new RecordTypeEntry(0, 0, 0, emptyList());
+        
+        OperationEntry operationEntry1 = new OperationEntry(0, "op1", new RecordMappingOperation(typeEntry), new RecordMappingOperation(typeEntry));
+        OperationEntry operationEntry2 = new OperationEntry(1, "op2", new RecordMappingOperation(typeEntry), new RecordMappingOperation(typeEntry));        
+        
+        ApiMappingScript script = new ApiMappingScript(singletonList(typeEntry), asList(operationEntry1, operationEntry2));
+        ApiMappingScriptCodec codec = new ApiMappingScriptCodec();
+        
+        byte[] expectedBytes = new byte[] {
+                // --- Type entries
+                0x00, 0x00, 0x00, 0x01, // Number of type entries
+                0x00, 0x00, 0x00, 0x0C, // Offset of the first type entry
+                0x00, 0x00, 0x00, 0x19, // Offset of the operation list
+                0x02, // Entry type for the first type (record type)
+                0x00, 0x00, 0x00, 0x00, // Type id of the first type
+                0x00, 0x00, 0x00, 0x00, // Data length of the first type
+                0x00, 0x00, 0x00, 0x00, // Number of field mappings
+                // --- Operation entries
+                0x00, 0x00, 0x00, 0x02, // Number of operation entries
+                0x00, 0x00, 0x00, 0x03, // Length of the operation name (in bytes)
+                0x6F, 0x70, 0x31, // Name of the operation
+                0x04, // Operation type of the parameter mapping operation (record mapping)
+                0x00, 0x00, 0x00, 0x00, // Type index of the record
+                0x04, // Operation type of the result mapping operation (record mapping)
+                0x00, 0x00, 0x00, 0x00, // Type index of the record
+                0x00, 0x00, 0x00, 0x03, // Length of the operation name (in bytes)
+                0x6F, 0x70, 0x32, // Name of the operation
+                0x04, // Operation type of the parameter mapping operation (record mapping)
+                0x00, 0x00, 0x00, 0x00, // Type index of the record
+                0x04, // Operation type of the result mapping operation (record mapping)
+                0x00, 0x00, 0x00, 0x00 // Type index of the record
         };
         
         byte[] scriptBytes = codec.encodeScript(script);
