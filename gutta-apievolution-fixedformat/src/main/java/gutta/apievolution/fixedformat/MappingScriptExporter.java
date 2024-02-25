6f8977b4a6212c9771f8747be3d1e9bc14acad33
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-02-25 11:39:13 +0100

    Addressed CheckStyle issues

--- gutta-apievolution-fixedformat/src/main/java/gutta/apievolution/fixedformat/MappingScriptExporter.java
+++ gutta-apievolution-fixedformat/src/main/java/gutta/apievolution/fixedformat/MappingScriptExporter.java
@@ -31,13 +31,19 @@ import static java.util.Objects.requireNonNull;
  * Tool to generate and export a mapping script to a file.
  */
 public class MappingScriptExporter {
-    
+
+    /**
+     * Entry point of the exporter tool.
+     * 
+     * @param arguments The command line arguments to use
+     * @throws IOException If an I/O error occurs
+     */
     public static void main(String[] arguments) throws IOException {
         Arguments parsedArguments = parseArguments(arguments);
-        
+
         new MappingScriptExporter().exportScript(parsedArguments);
     }
-    
+
     private static Arguments parseArguments(String[] arguments) {
         String consumerApiName = null;
         List<String> providerRevisionNames = new ArrayList<>();
@@ -46,42 +52,42 @@ public class MappingScriptExporter {
         String outputScriptName = null;
         Set<Integer> supportedRevisions = new HashSet<>();
         MappingDirection mappingDirection = null;
-    
+
         int currentArgumentIndex = 0;
         while (currentArgumentIndex < arguments.length) {
             String currentArgument = arguments[currentArgumentIndex++];
-            
+
             switch (currentArgument) {
             case "-c":
             case "--consumer-api":
                 consumerApiName = arguments[currentArgumentIndex++];
                 break;
-                
+
             case "-n":
             case "--referenced-api":
                 referencedApiName = arguments[currentArgumentIndex++];
                 break;
-                
+
             case "-r":
             case "--referenced-revision":
                 referencedRevision = Integer.parseInt(arguments[currentArgumentIndex++]);
                 break;
-                
+
             case "-o":
             case "--output":
                 outputScriptName = arguments[currentArgumentIndex++];
                 break;
-                
+
             case "-s":
             case "--supported-revision":
                 supportedRevisions.add(Integer.valueOf(arguments[currentArgumentIndex++]));
                 break;
-                
+
             case "-p":
             case "--provider-revision":
                 providerRevisionNames.add(arguments[currentArgumentIndex++]);
                 break;
-                
+
             case "-t":
             case "--type":
                 String type = arguments[currentArgumentIndex++];
@@ -89,103 +95,105 @@ public class MappingScriptExporter {
                 case "consumer":
                     mappingDirection = MappingDirection.CONSUMER_TO_PROVIDER;
                     break;
-                    
+
                 case "provider":
                     mappingDirection = MappingDirection.PROVIDER_TO_CONSUMER;
                     break;
-                    
+
                 default:
                     throw new IllegalArgumentException("Unknown type '" + type + "',  must be 'consumer' or 'provider'.");
                 }
-                
+                break;
+
             default:
                 break;
             }
         }
-        
+
         requireNonNull(consumerApiName, "No consumer API given.");
         requireNonNull(referencedApiName, "No referenced API name given.");
         requireNonNull(mappingDirection, "No type given.");
-        requireNonNull(outputScriptName, "No output script name given.");        
-                
-        return new Arguments(consumerApiName, referencedApiName, referencedRevision, providerRevisionNames, supportedRevisions, mappingDirection, outputScriptName);
+        requireNonNull(outputScriptName, "No output script name given.");
+
+        return new Arguments(consumerApiName, referencedApiName, referencedRevision, providerRevisionNames, supportedRevisions, mappingDirection,
+                outputScriptName);
     }
-    
+
     void exportScript(Arguments arguments) {
         // Load the consumer API
         ConsumerApiDefinition consumerApi = this.loadConsumerApi(arguments);
-        
+
         // Load the provider revision history
         RevisionHistory revisionHistory = this.loadRevisionHistory(arguments);
-        
+
         // Resolve the APIs against each other and generate the mapping script
-        DefinitionResolution definitionResolution = new DefinitionResolver().resolveConsumerDefinition(revisionHistory, arguments.supportedRevisions, consumerApi);        
+        DefinitionResolution definitionResolution = new DefinitionResolver().resolveConsumerDefinition(revisionHistory, arguments.supportedRevisions,
+                consumerApi);
         ApiMappingScript mappingScript = new ApiMappingScriptGenerator().generateMappingScript(definitionResolution, arguments.mappingDirection);
-                
+
         // Write the mapping script
         this.writeScript(arguments.outputScriptName, mappingScript);
     }
-    
+
     private ConsumerApiDefinition loadConsumerApi(Arguments arguments) {
         String consumerApiName = arguments.consumerApiName;
-        
+
         try (InputStream inputStream = this.openInputStream(consumerApiName)) {
             return ConsumerApiLoader.loadFromStream(inputStream, consumerApiName, arguments.referencedApiName, arguments.referencedRevision);
         } catch (IOException e) {
-            throw new ExportException("Error loading consumer API '" + consumerApiName + "'.", e); 
+            throw new ExportException("Error loading consumer API '" + consumerApiName + "'.", e);
         }
     }
-    
+
     InputStream openInputStream(String streamName) throws IOException {
         return new FileInputStream(streamName);
     }
-    
+
     private RevisionHistory loadRevisionHistory(Arguments arguments) {
-        List<NamedInputStream> inputStreams = arguments.providerRevisionNames.stream()
-                .map(streamName -> new NamedInputStream(streamName, () -> {
-                    try {
-                        return this.openInputStream(streamName);
-                    } catch (IOException e) {
-                        throw new ExportException("Error loading provider API '" + streamName + "'.", e);
-                    }
-                }))
-                .collect(Collectors.toList());
-        
+        List<NamedInputStream> inputStreams = arguments.providerRevisionNames.stream().map(streamName -> new NamedInputStream(streamName, () -> {
+            try {
+                return this.openInputStream(streamName);
+            } catch (IOException e) {
+                throw new ExportException("Error loading provider API '" + streamName + "'.", e);
+            }
+        })).collect(Collectors.toList());
+
         List<ProviderApiDefinition> providerApis = ProviderApiLoader.loadHistoryFromStreams(IntegerRange.unbounded(), false, inputStreams);
         return new RevisionHistory(providerApis);
-    }   
-    
+    }
+
     private void writeScript(String fileName, ApiMappingScript script) {
         byte[] scriptBytes = new ApiMappingScriptCodec().encodeScript(script);
-        
+
         try (OutputStream outputStream = this.openOutputStream(fileName)) {
             outputStream.write(scriptBytes);
         } catch (IOException e) {
             throw new ExportException("Error writing script to '" + fileName + "'.", e);
         }
     }
-    
+
     OutputStream openOutputStream(String outputName) throws IOException {
         return new FileOutputStream(outputName);
     }
-    
+
     static class Arguments {
-        
+
         public final String consumerApiName;
-        
+
         public final String referencedApiName;
-        
+
         public final int referencedRevision;
-        
+
         public final List<String> providerRevisionNames;
-        
+
         public final Set<Integer> supportedRevisions;
-        
+
         public final MappingDirection mappingDirection;
-        
+
         public final String outputScriptName;
-        
-        public Arguments(String consumerApiName, String referencedApiName, int referencedRevision, List<String> providerRevisionNames, Set<Integer> supportedRevisions, MappingDirection mappingDirection, String outputScriptName) {
+
+        public Arguments(String consumerApiName, String referencedApiName, int referencedRevision, List<String> providerRevisionNames,
+                Set<Integer> supportedRevisions, MappingDirection mappingDirection, String outputScriptName) {
             this.consumerApiName = consumerApiName;
             this.referencedApiName = referencedApiName;
             this.referencedRevision = referencedRevision;
@@ -194,17 +202,17 @@ public class MappingScriptExporter {
             this.mappingDirection = mappingDirection;
             this.outputScriptName = outputScriptName;
         }
-        
+
     }
-    
+
     static class ExportException extends RuntimeException {
-        
+
         private static final long serialVersionUID = 527635247776530245L;
 
         public ExportException(String message, Throwable cause) {
             super(message, cause);
         }
-        
+
     }
 
 }
