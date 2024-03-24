235520b8be808d6ec5cae0e668dffe45f5fce337
Holger Knoche <hkn@informatik.uni-kiel.de>
2024-03-24 18:06:03 +0100

    Added serial version uid

--- gutta-apievolution-json/src/test/java/gutta/apievolution/json/consumer/ConsumerTestException.java
+++ gutta-apievolution-json/src/test/java/gutta/apievolution/json/consumer/ConsumerTestException.java
@@ -2,6 +2,8 @@ package gutta.apievolution.json.consumer;
 
 public class ConsumerTestException extends RuntimeException {
     
+    private static final long serialVersionUID = -7450054012675597283L;
+    
     private final ConsumerTestExceptionData data;
     
     public ConsumerTestException(ConsumerTestExceptionData data) {
